7c6f41c68db8db5228192101d0d525b47e97a657
Milton <tomilton@hotmail.com>
2025-01-24 19:15:09 -0500

    adds base project

---
+++ ms-users/src/main/java/org/msanchez/springcloud/ms/users/MsUsersApplication.java
@@ -0,0 +1,13 @@
+package org.msanchez.springcloud.ms.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsersApplication.class, args);
+	}
+
+}
---
+++ ms-users/src/test/java/org/msanchez/springcloud/ms/users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msanchez.springcloud.ms.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
