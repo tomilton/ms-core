aaef0c0bb8e12f3855a80dd6228f3713f7dc62ee
Milton <tomilton@hotmail.com>
2025-01-28 11:35:09 -0500

    adds arg

--- ms-users/build.sh
+++ ms-users/build.sh
@@ -1,4 +1,6 @@
 ./gradlew clean build -x test &&
-docker build -t ms-user:1.0.0 . &&
+#docker build -t ms-user:1.0.0 . &&
+docker build -t ms-user:1.0.0 --build-arg PORT_APP=8080 . &&
 #docker run --add-host=host.docker.internal:host-gateway -p 8001:8090 --env PORT=8090 ms-user:1.0.0
-docker run --add-host=host.docker.internal:host-gateway -p 8001:8090 --env-file \.env ms-user:1.0.0
+#docker run --add-host=host.docker.internal:host-gateway -p 8001:8090 --env-file \.env ms-user:1.0.0
+docker run --add-host=host.docker.internal:host-gateway -p 8001:8080 ms-user:1.0.0
