2e15b407c5769d927fb5b2cfb37270ccbe0fbe84
Milton <tomilton@hotmail.com>
2025-01-28 14:10:40 -0500

    adds env variable for client rest

--- ms-courses/src/main/java/org/msanchez/springcloud/ms/courses/clients/UsuarioClientRest.java
+++ ms-courses/src/main/java/org/msanchez/springcloud/ms/courses/clients/UsuarioClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "ms-user", url = "ms-user:8001/api/user")
+@FeignClient(name = "ms-user", url = "${ms.users.url}")
 public interface UsuarioClientRest {
 
   @GetMapping("/{id}")
