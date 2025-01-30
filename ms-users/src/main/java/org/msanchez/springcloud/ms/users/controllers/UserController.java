d974ca6a5e24c32532b7a529b87a90b2b966d70f
Milton <tomilton@hotmail.com>
2025-01-30 08:24:16 -0500

    adds endpoint crash

--- ms-users/src/main/java/org/msanchez/springcloud/ms/users/controllers/UserController.java
+++ ms-users/src/main/java/org/msanchez/springcloud/ms/users/controllers/UserController.java
@@ -3,11 +3,15 @@ package org.msanchez.springcloud.ms.users.controllers;
 import lombok.AllArgsConstructor;
 import org.msanchez.springcloud.ms.users.model.entity.User;
 import org.msanchez.springcloud.ms.users.services.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -16,10 +20,16 @@ import java.util.Optional;
 public class UserController {
 
   private UserService userService;
+  private ApplicationContext context;
+
+  @GetMapping("/crash")
+  public void crash() {
+    ((ConfigurableApplicationContext) context).close();
+  }
 
   @GetMapping
-  public List<User> findAll() {
-    return userService.findAll();
+  public Map<String, List<User>> findAll() {
+    return Collections.singletonMap("users", userService.findAll());
   }
 
   @GetMapping("/{id}")
