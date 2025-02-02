54a9cd0729635c8c5d5b408be85b5bc77f09da5f
Milton <tomilton@hotmail.com>
2025-02-02 10:37:06 -0500

    feat: adds oauth

---
+++ oauth/src/test/java/org/msanchez/springcloud/ms/oauth/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msanchez.springcloud.ms.oauth.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
